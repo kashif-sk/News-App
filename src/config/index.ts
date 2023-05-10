4c6be3df6ea76a380c3bbaa1d4dd0ef1b198158c
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 00:04:03 +0530

    feat: multi-language support - supportedLanguages, rtlLanguageCodes, defaultLanguage added in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,13 @@
+export const supportedLanguages = [
+  {languageCode: 'en', displayName: 'English'},
+  {languageCode: 'ar', displayName: 'عربي'},
+] as const;
+
+export const rtlLanguageCodes: (typeof supportedLanguages)[number]['languageCode'][] =
+  ['ar'];
+
+export const defaultLanguage: (typeof supportedLanguages)[number]['languageCode'] =
+  'en';
+
 export const lightColorMode = 'light';
 export const darkColorMode = 'dark';
