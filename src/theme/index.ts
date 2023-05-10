a3ec71b6da5d6ec36b303a959700e9cc9be16621
Kashif Shaikh <kashifs@valuefy.com>
2023-05-10 23:39:05 +0530

    feat: colorModeManager added to persist theme state

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,25 @@
-import {extendTheme} from 'native-base';
+import {ColorMode, extendTheme} from 'native-base';
+import {darkColorMode, lightColorMode} from '../config';
+import {localStorage} from '../utils/localStorage';
 
 export const customTheme = extendTheme({});
+
+export const colorModeManager = {
+  get: async () => {
+    try {
+      const val = await localStorage.get('@color-mode');
+      return val === darkColorMode ? darkColorMode : lightColorMode;
+    } catch (e) {
+      return lightColorMode;
+    }
+  },
+  set: async (value: ColorMode) => {
+    try {
+      const strValue = value ? value.toString() : '';
+      localStorage.set('@color-mode', strValue);
+    } catch (e) {}
+  },
+};
 
 // 2. Get the type of the CustomTheme
 type CustomThemeType = typeof customTheme;
