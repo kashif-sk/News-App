87ed4f21dc88d72abf5b328d854eaf5be3d6c119
Kashif Shaikh <kashifs@valuefy.com>
2023-05-10 23:36:56 +0530

    feat: customTheme exported, typescript support added for customTheme

---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import {extendTheme} from 'native-base';
+
+export const customTheme = extendTheme({});
+
+// 2. Get the type of the CustomTheme
+type CustomThemeType = typeof customTheme;
+
+// 3. Extend the internal NativeBase Theme
+declare module 'native-base' {
+  interface ICustomTheme extends CustomThemeType {}
+}
