957191e4a91f1f96c6543fac4c027c4c29316549
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 18:30:18 +0530

    feat: performance-improvement - items fetched in batches, pagination implemented

--- src/api/useFetchArticles.ts
+++ src/api/useFetchArticles.ts
@@ -7,6 +7,9 @@ import {ArticlesData, FetchArticlesResponse} from '../models/Articles';
 import useAppStore from '../store';
 import {getQueryString} from '../utils/helper';
 
+const PageSize = 48;
+const MaxPageAccessible = Math.floor(100 / PageSize);
+
 const useFetchArticles = ({topic}: useFetchArticlesParams) => {
   const {language} = useAppStore();
   const {t} = useAppTranslation();
@@ -14,11 +17,16 @@ const useFetchArticles = ({topic}: useFetchArticlesParams) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const [articlesData, setArticlesData] = useState<ArticlesData>([]);
+  const [page, setPage] = useState(1);
+  const [loadingMoreItems, setLoadingMoreItems] = useState(false);
+  const [isAllArticlesFetched, setIsAllArticlesFetched] = useState(false);
 
   const fetchArticles = useCallback(async () => {
     try {
       setError('');
-      setLoading(true);
+      if (page === 1) {
+        setLoading(true);
+      }
       const todaysDate = new Date().getDate();
       const fromDate = new Date(
         new Date().setDate(todaysDate - 30),
@@ -29,12 +37,27 @@ const useFetchArticles = ({topic}: useFetchArticlesParams) => {
         from: fromDate,
         sortBy: 'publishedAt',
         language,
+        pageSize: PageSize,
+        page,
       };
 
       const url = `everything?${getQueryString(queryParams)}`;
       const {data} = await axios.get<FetchArticlesResponse>(url);
       if (data.status === 'ok') {
-        setArticlesData(data.articles);
+        let articlesDataCount = 0;
+        page === 1
+          ? setArticlesData(data.articles)
+          : setArticlesData(prevData => {
+              const newData = [...prevData, ...data.articles];
+              articlesDataCount = newData.length;
+              return newData;
+            });
+        if (
+          articlesDataCount >= data.totalResults ||
+          page >= MaxPageAccessible
+        ) {
+          setIsAllArticlesFetched(true);
+        }
       } else {
         setError(data.message);
       }
@@ -42,8 +65,9 @@ const useFetchArticles = ({topic}: useFetchArticlesParams) => {
       setError(t('defaultError'));
     } finally {
       setLoading(false);
+      setLoadingMoreItems(false);
     }
-  }, [language, t, topic]);
+  }, [language, page, t, topic]);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedFetchArticles = useCallback(debounce(fetchArticles, 500), [
@@ -54,10 +78,20 @@ const useFetchArticles = ({topic}: useFetchArticlesParams) => {
     debouncedFetchArticles();
   }, [debouncedFetchArticles]);
 
+  const loadMoreArticles = useCallback(() => {
+    if (!loading && !loadingMoreItems) {
+      setLoadingMoreItems(true);
+      setPage(prevPage => prevPage + 1);
+    }
+  }, [loading, loadingMoreItems]);
+
   return {
     loading,
+    loadingMoreItems,
     error,
     articlesData,
+    isAllArticlesFetched,
+    loadMoreArticles,
   };
 };
 
