56363400cd28f3806c343ed60312acf5500eb2fe
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 16:37:29 +0530

    feat: flatlistContentContainer styles added

---
+++ src/screens/ArticleList/styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {ArticleListStyle} from './types';
+
+const styles = StyleSheet.create<ArticleListStyle>({
+  flatlistContentContainer: {
+    alignItems: 'center',
+  },
+});
+
+export default styles;
