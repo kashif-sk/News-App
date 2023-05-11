5b21274ca3c0fa0631c73c4df30fa9ff3afd511a
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 18:59:15 +0530

    fix: interceptor imported at root

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './localization';
 import React from 'react';
 import {NativeBaseProvider, Spinner} from 'native-base';
+import './api/interceptor';
 import {colorModeManager, customTheme} from './theme';
 import ArticleList from './screens/ArticleList';
 import useAppStore from './store';
