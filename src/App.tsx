11fa4e67a1499c397cc872930fea53b509270f8f
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 16:13:11 +0530

    feat: ArticleList page displayed at root instead of default app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {NativeBaseProvider, Box} from 'native-base';
+import {NativeBaseProvider} from 'native-base';
 import './localization';
 import {colorModeManager, customTheme} from './theme';
+import ArticleList from './screens/ArticleList';
 
 export default function App() {
   return (
     <NativeBaseProvider colorModeManager={colorModeManager} theme={customTheme}>
-      <Box>Hello world</Box>
+      <ArticleList />
     </NativeBaseProvider>
   );
 }
