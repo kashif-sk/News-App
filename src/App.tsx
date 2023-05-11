4d6faa750f9e8abf50d3807fe0dab640b2abf47e
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 19:00:05 +0530

    fix: flex 1 not working at root on web - App root wrapped with Box of variant container, window height assigned in case of web

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './localization';
 import React from 'react';
-import {NativeBaseProvider, Spinner} from 'native-base';
+import {Platform, useWindowDimensions} from 'react-native';
+import {Box, NativeBaseProvider, Spinner} from 'native-base';
 import './api/interceptor';
 import {colorModeManager, customTheme} from './theme';
 import ArticleList from './screens/ArticleList';
@@ -8,9 +9,15 @@ import useAppStore from './store';
 
 export default function App() {
   const {language} = useAppStore();
+  const {height} = useWindowDimensions();
+
   return (
     <NativeBaseProvider colorModeManager={colorModeManager} theme={customTheme}>
-      {language == null ? <Spinner size="lg" /> : <ArticleList />}
+      <Box
+        variant="container"
+        minHeight={Platform.OS === 'web' ? height : undefined}>
+        {language == null ? <Spinner size="lg" /> : <ArticleList />}
+      </Box>
     </NativeBaseProvider>
   );
 }
