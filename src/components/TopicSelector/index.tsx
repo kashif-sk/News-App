2f9e2bd079ad3927cba129df971330f7ee2e1fcc
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 20:52:04 +0530

    fix: TopicSelector Language hardcoded - key used for api purpose, labelKey used for displaying translated label

--- src/components/TopicSelector/index.tsx
+++ src/components/TopicSelector/index.tsx
@@ -1,6 +1,7 @@
 import {Pressable, CheckIcon, Row, Text} from 'native-base';
 import React from 'react';
 import {topics} from '../../config';
+import {useAppTranslation} from '../../localization';
 import {TopicSelectorProps} from './types';
 
 const TopicSelector = ({
@@ -8,22 +9,23 @@ const TopicSelector = ({
   selectedTopic,
   onTopicChange,
 }: TopicSelectorProps): JSX.Element => {
+  const {t} = useAppTranslation();
   return (
     <Row flexWrap="wrap" p="2" pb="0">
       {topics.map(topic => {
-        const isSelected = selectedTopic === topic;
+        const isSelected = selectedTopic === topic.key;
         return (
           <Pressable
-            key={topic}
+            key={topic.key}
             variant="chip"
             disabled={disabled}
-            onPress={() => onTopicChange(topic)}
+            onPress={() => onTopicChange(topic.key)}
             opacity={disabled ? 0.5 : 1}>
             {isSelected && (
               <CheckIcon color="primary.800" _dark={{color: 'primary.300'}} />
             )}
             <Text variant="text14_400" px="2">
-              {topic}
+              {t(topic.labelKey)}
             </Text>
           </Pressable>
         );
