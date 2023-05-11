2bc3ee882ca5c3919aeaf8aa60decc44e07f2f88
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 20:14:17 +0530

    type of disabled set to boolean & optional

--- src/components/TopicSelector/types.ts
+++ src/components/TopicSelector/types.ts
@@ -1,6 +1,7 @@
 import {topics} from '../../config';
 
 export type TopicSelectorProps = {
+  disabled?: boolean;
   selectedTopic: (typeof topics)[number];
   onTopicChange: (topic: (typeof topics)[number]) => void;
 };
