614126f8df792d82dc3840d820cf835a2aa52aeb
Kashif Shaikh <kashifs@valuefy.com>
2023-05-11 16:14:05 +0530

    fix: language not initialized - Spinner displayed before rendering actual app to ensure language is initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import {NativeBaseProvider} from 'native-base';
 import './localization';
+import React from 'react';
+import {NativeBaseProvider, Spinner} from 'native-base';
 import {colorModeManager, customTheme} from './theme';
 import ArticleList from './screens/ArticleList';
+import useAppStore from './store';
 
 export default function App() {
+  const {language} = useAppStore();
   return (
     <NativeBaseProvider colorModeManager={colorModeManager} theme={customTheme}>
-      <ArticleList />
+      {language == null ? <Spinner size="lg" /> : <ArticleList />}
     </NativeBaseProvider>
   );
 }
